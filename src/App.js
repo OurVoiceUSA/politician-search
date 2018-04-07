09d91abe5914e89519fae380c6f9a8f8efbabe8f
Corey Henderson <corman@cormander.com>
2018-04-07 13:13:58 -0600

    updated layout

--- src/App.js
+++ src/App.js
@@ -73,13 +73,39 @@ export default class App extends PureComponent {
         <View style={{marginRight: 10}}>
           <img style={{width: 150, resizeMode: 'center'}} alt="" src={(obj.photo_url?obj.photo_url:'https://ourvoiceusa.org/wp-content/uploads/2017/12/thinker-400x250.jpg')} />
         </View>
-        <View>
+        <View style={{width: 425}}>
           <h2>{obj.name}</h2>
-          <h3>{obj.divisionName}</h3>
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{margin: 10}}>{obj.phone}</Text>
-            <Text style={{margin: 10}}>{obj.email}</Text>
-            <Text style={{margin: 10}}>{obj.address}</Text>
+          <h3>{obj.office}, {obj.divisionName}</h3>
+          <View style={{flex: 1}}>
+            <Text>
+              {this._partyNameFromKey(obj.party)}
+            </Text>
+
+            <View style={{flex: 1, marginTop: 7}}>
+              <View style={{flexDirection: 'row'}}>
+                <View style={{marginRight: 5}}>
+                  <Text>Phone:</Text>
+                </View>
+                <View>
+                  <Text>{(obj.phone?obj.phone:"N/A")}</Text>
+                </View>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <View style={{marginRight: 7}}>
+                  <Text>Email:</Text>
+                </View>
+                <View>
+                  <Text>{(obj.email?obj.email:"N/A")}</Text>
+                </View>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text>Mailing Address:</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text>{(obj.address?obj.address:"N/A")}</Text>
+              </View>
+            </View>
+
           </View>
           <View style={{flexDirection: 'row'}}>
             {this.linkSM(obj, 'phone', 'phone-square', '#5BC236')}
@@ -90,10 +116,31 @@ export default class App extends PureComponent {
             {this.linkSM(obj, 'wikipedia_id', 'wikipedia-w', '#000000')}
             {this.linkSM(obj, 'url', 'globe', '#008080')}
           </View>
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{margin: 10}}>{JSON.stringify(obj.data_sources)}</Text>
-          </View>
         </View>
+        <View>
+          <Text style={{margin: 10, marginLeft: 0}}>Data Sources for this record:</Text>
+          {this.displayDataSources(obj.data_sources)}
+        </View>
+      </View>
+    );
+  }
+
+  displayDataSources(refs) {
+    let items = [];
+
+    for (let r in refs) {
+      let ref = refs[r];
+
+      items.push(
+        <View key={r}>
+          <Text><a href={ref.url} target="_blank">{ref.name}</a>{(ref.id?' ('+ref.id+')':'')}</Text>
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        { items }
       </View>
     );
   }
@@ -102,6 +149,17 @@ export default class App extends PureComponent {
     return (
       <Icon name={icon} size='2x' style={{margin: 10, color: (obj[sm] ? color : '#e3e3e3')}} />
     );
+  }
+
+  _partyNameFromKey(party) {
+    switch (party) {
+      case 'D': return 'Democrat';
+      case 'R': return 'Republican';
+      case 'I': return 'Independent';
+      case 'G': return 'Green';
+      case 'L': return 'Libertarian';
+      default: return '';
+    }
   }
 
   displayPoliticiansByAddress(apiData) {
